77d5a1eb6aa94b615ff07dde8ff3627f6ba7dd54
osamaeshmilh <osamaeshmilh@gmail.com>
2022-06-11 20:45:29 +0200

    material

--- src/main/java/ly/gov/eidc/archive/service/util/FileTools.java
+++ src/main/java/ly/gov/eidc/archive/service/util/FileTools.java
@@ -8,6 +8,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.Random;
 import org.apache.commons.io.FileUtils;
 import org.springframework.http.MediaType;
@@ -50,8 +51,20 @@ public class FileTools {
             PdfReader pdfReader = new PdfReader(pdf);
             PdfStamper pdfStamper = new PdfStamper(pdfReader, invoiceWithBarCode);
             Barcode128 barcode128 = new Barcode128();
-            barcode128.setCode("EIDC_" + fileName);
+            barcode128.setCode("EIDC_" + fileName.replaceAll(".pdf", ""));
             barcode128.setCodeType(Barcode.CODE128);
+
+            HashMap<String, String> info = pdfReader.getInfo();
+            info.put("Title", fileName.replaceAll(".pdf", ""));
+            info.put("Author", "Center for Economic Information and Documentation, Libyan Ministry of Economy");
+            info.put("Subject", "مركز المعلومات والتوثيق الاقتصادي");
+            info.put("Keywords", "EIDC, P.O.Box 82280 Tripoli, Libya, https://eidc.gov.ly");
+            info.put("Creator", "EIDC");
+            info.put("Producer", null);
+            info.put("CreationDate", null);
+            info.put("ModDate", LocalDate.now().toString());
+            info.put("Trapped", null);
+            pdfStamper.setMoreInfo(info);
             for (int j = 1; j <= pdfReader.getNumberOfPages(); j++) {
                 PdfContentByte content = pdfStamper.getOverContent(j);
                 Rectangle mediabox = pdfReader.getPageSize(j);
